acf31a0690f2ee3c2dcba3bf7c029086b3d7a9c5
pknelson01 <pk.elliott11@gmail.com>
2022-06-23 20:37:44 -0600

    new content

--- code-along.py
+++ code-along.py
@@ -1307,73 +1307,132 @@ use this in terminal: python3 code-along.py
 
 # How to Build a Custom Iterator Class in Python (after un-commenting code below, fix the indentation or code wont run)
 
-""" 
-An Iterator will give you complete control to navigate through the list.
-"""
-class Lineup:
-    def __init__(self, players):
-        self.players = players
-        self.last_player_index = (len(self.players) - 1)
+        # """ 
+        # An Iterator will give you complete control to navigate through the list.
+        # """
+        # class Lineup:
+        #     def __init__(self, players):
+        #         self.players = players
+        #         self.last_player_index = (len(self.players) - 1)
 
-    def __iter__(self):
-        self.n = 0
-        return self
+        #     def __iter__(self):
+        #         self.n = 0
+        #         return self
 
-    def get_player(self, n):
-        return self.players[n]
+        #     def get_player(self, n):
+        #         return self.players[n]
 
-    def __next__(self):
-        if self.n < self.last_player_index:
-            player = self.get_player(self.n)
-            self.n += 1
-            return player
-        elif self.n == self.last_player_index:
-            player = self.get_player(self.n)
-            self.n = 0
-            return player
+        #     def __next__(self):
+        #         if self.n < self.last_player_index:
+        #             player = self.get_player(self.n)
+        #             self.n += 1
+        #             return player
+        #         elif self.n == self.last_player_index:
+        #             player = self.get_player(self.n)
+        #             self.n = 0
+        #             return player
 
-astros = [
-    'Springer',
-    'Bregman',
-    'Altuve',
-    'Correa',
-    'Reddick',
-    'Gonzalez',
-    'McCann',
-    'Davis',
-    'Tucker'
-]
+        # astros = [
+        #     'Springer',
+        #     'Bregman',
+        #     'Altuve',
+        #     'Correa',
+        #     'Reddick',
+        #     'Gonzalez',
+        #     'McCann',
+        #     'Davis',
+        #     'Tucker'
+        # ]
 
-astros_lineup = Lineup(astros)
-process = iter(astros_lineup)
+        # astros_lineup = Lineup(astros)
+        # process = iter(astros_lineup)
 
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
-print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
+        # print(next(process))
 
+# Overview of Iterators vs Generators in Python (after un-commenting code below, fix the indentation or code wont run)
 
-#  (after un-commenting code below, fix the indentation or code wont run)
+        # class InfiniteLineup:
+        #     def __init__(self, players):
+        #         self.players = players
 
-#  (after un-commenting code below, fix the indentation or code wont run)
+        #     def lineup(self):
+        #         linup_max = len(self.players)
+        #         idx = 0
+
+        #         while True:
+        #             if idx < linup_max:
+        #                 yield self.players[idx]
+        #             else:
+        #                 idx = 0
+        #                 yield self.players[idx]
+
+        #             idx += 1
+
+        #     def __repr__(self):
+        #         return f'<InfiniteLineup({self.players})'
+        #     def __str__(self):
+        #         return f'<InfiniteLineup with the players : {self.players}'
+
+        # astros = [
+        #         'Springer',
+        #         'Bregman',
+        #         'Altuve',
+        #         'Correa',
+        #         'Reddick',
+        #         'Gonzalez',
+        #         'McCann',
+        #         'Davis',
+        #         'Tucker'
+        # ]
+
+        # full_lineup = InfiniteLineup(astros)
+        # astros_lineup = full_lineup.lineup()
+
+        # print(next(astros_lineup))
+        # print(next(astros_lineup))
+        # print(next(astros_lineup))
+
+        # print(repr(astros_lineup))
+        # print(str(astros_lineup))
+
+# Class vs Instance Attributes in Python (after un-commenting code below, fix the indentation or code wont run)
+
+# class Website:
+#     def __init__(self, title):
+#         self.title = title
+
+# ws = Website('My Website Title') 
+# print(ws.__dict__)
+
+# ws_two = Website('My Awesome Website Title') 
+# print(ws_two.__dict__)
+
+class DifferntWebsite:
+    title = 'My Class Title'
+
+dw = DifferntWebsite()
+print(dw.__dict__)
 
 #  (after un-commenting code below, fix the indentation or code wont run)
 
