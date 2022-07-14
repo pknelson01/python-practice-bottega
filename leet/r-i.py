cbf497ddee258dd0313114347c5f7a662098e50f
pknelson01 <pk.elliott11@gmail.com>
2022-07-14 17:26:23 -0600

    new

--- leet/r-i.py
+++ leet/r-i.py
@@ -1,29 +1,20 @@
 # Roman-to-Integer
+def roman_to_int(x):
+    roman_ints = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'c': 100,
+    'D': 500,
+    'M': 1000,
+    }
+    index_val = 0
+    for i in range(len(x)):
+        if i > 0 and roman_ints[x[i]] > roman_ints[x[i - 1]]:
+            index_val += roman_ints[x[i]] - 2 * roman_ints[x[i - 1]]
+    else:
+        index_val +=roman_ints[x[i]]
+    return index_val
 
-roman_ints = {
-'I': 1,
-'V': 5,
-'X': 10,
-'L': 50,
-'c': 100,
-'D': 500,
-'M': 1000,
-}
-
-x = 'IV'
-
-total = 0
-for char in x:
-    if char in roman_ints.keys():
-        if char == 'I' and char[char + 1] == 'V': # char == 'X' and char == 'C'
-            i = 4
-        elif char == 'V' and char[char - 1] == 'I':
-            continue
-        elif char =='I' and char[char + 1] == "X":
-            i = 9
-
-        else:
-            i = roman_ints[char]
-            total += i
-else:
-    print(total)
+print(roman_to_int('MMDCCLXIV'))
